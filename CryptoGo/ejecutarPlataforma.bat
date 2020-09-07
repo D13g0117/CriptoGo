ae7a4dfe84cb2d6efcd1cde5c22a1a89cb6c6c38
Diego <diegogg117@gmail.com>
2020-09-07 19:41:37 +0200

    Añadido fichero de ejecución de la plataforma

---
+++ CryptoGo/ejecutarPlataforma.bat
@@ -0,0 +1,31 @@
+@echo off
+set extra=
+FOR /L %%i IN (200,1,290) DO (
+	set extra=!extra!;C:\Java\jdk1.8.0_%%i\bin;C:\Program Files\Java\jdk1.8.0_%%i\bin
+)
+
+set classpath=ScwJadex.jar;^
+src\lib\Base64.jar;^
+src\lib\GraphLayout.jar;^
+src\lib\bcel.jar;^
+src\lib\http.jar;^
+src\lib\iiop.jar;^
+src\lib\jade.jar;^
+src\lib\jadeTools.jar;^
+src\lib\jadex_examples.jar;^
+src\lib\jadex_jadeadapter.jar;^
+src\lib\jadex_rt.jar;^
+src\lib\jadex_standalone.jar;^
+src\lib\jadex_tools.jar;^
+src\lib\jhall.jar;^
+src\lib\jibx-bind.jar;^
+src\lib\jibx-extras.jar;^
+src\lib\jibx-run.jar;^
+src\lib\xpp3.jar;^
+src;^
+src\lib\commons-commons\commons-codec-1.3.jar
+
+java jadex.adapter.standalone.Platform 
+
+pause 
+
